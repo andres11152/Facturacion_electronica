4cd387d6dc34dda9d30e5d9760d30a0390fc8d24
andresbetancourto <andres11152@gmail.com>
2023-04-05 22:25:49 -0500

    Agregando la clase Compra y DetalleFacturaQ

---
+++ src/model/Compra.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Compra {
+    
+    // Atributos
+    private Cliente cliente;
+    private Date fecha;
+    private List<DetalleFactura> detalles;
+    private double total;
+    
+    // Constructor
+    public Compra(Cliente cliente) {
+        this.cliente = cliente;
+        this.fecha = new Date();
+        this.detalles = new ArrayList<>();
+        this.total = 0.0;
+    }
+    
+    // Métodos
+    
+    // Agrega un detalle a la compra
+    public void agregarDetalle(DetalleFactura detalle) {
+        detalles.add(detalle);
+        total += detalle.getSubtotal();
+    }
+    
+    // Remueve un detalle de la compra
+    public void removerDetalle(DetalleFactura detalle) {
+        detalles.remove(detalle);
+        total -= detalle.getSubtotal();
+    }
+    
+    // Devuelve la lista de detalles de la compra
+    public List<DetalleFactura> getDetalles() {
+        return this.detalles;
+    }
+    
+    // Devuelve el cliente que realizó la compra
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+    
+    // Devuelve la fecha de la compra
+    public Date getFecha() {
+        return this.fecha;
+    }
+    
+    // Devuelve el total de la compra
+    public double getTotal() {
+        return this.total;
+    }
+    
+    // Devuelve una representación en String de la compra
+    @Override
+    public String toString() {
+        return "Compra[cliente=" + cliente.getNombre() + ", fecha=" + fecha + ", total=" + total + "]";
+    }
+}
---
+++ src/model/Factura.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Factura {
+    
+    // Atributos
+    private int id;
+    private Cliente cliente;
+    private List<DetalleFactura> detalles;
+    private LocalDate fecha;
+    private double total;
+    
+    // Constructor
+    public Factura(int id, Cliente cliente, List<DetalleFactura> detalles, LocalDate fecha, double total) {
+        this.id = id;
+        this.cliente = cliente;
+        this.detalles = detalles;
+        this.fecha = fecha;
+        this.total = total;
+    }
+    
+    // Métodos
+    
+    // Devuelve el id de la factura
+    public int getId() {
+        return this.id;
+    }
+    
+    // Devuelve el cliente asociado a la factura
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+    
+    // Devuelve la lista de detalles de la factura
+    public List<DetalleFactura> getDetalles() {
+        return this.detalles;
+    }
+    
+    // Devuelve la fecha de la factura
+    public LocalDate getFecha() {
+        return this.fecha;
+    }
+    
+    // Devuelve el total de la factura
+    public double getTotal() {
+        return this.total;
+    }
+    
+    // Agrega un nuevo detalle a la factura
+    public void agregarDetalle(DetalleFactura detalle) {
+        this.detalles.add(detalle);
+    }
+    
+    // Calcula y actualiza el total de la factura
+    public void calcularTotal() {
+        double subtotal = 0.0;
+        for (DetalleFactura detalle : detalles) {
+            subtotal += detalle.getSubtotal();
+        }
+        this.total = subtotal;
+    }
+    
+    // Devuelve una representación en String de la factura
+    @Override
+    public String toString() {
+        return "Factura[id=" + id + ", cliente=" + cliente.getNombre() + ", fecha=" + fecha + ", total=" + total + "]";
+    }
+}
