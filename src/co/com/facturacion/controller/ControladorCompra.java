66128f2c4f4bf23f1baf92733ee26e2894d24f7f
andresbetancourto <andres11152@gmail.com>
2023-04-06 11:37:48 -0500

    Se crea método ControladorCompra

--- src/co/com/facturacion/controller/ControladorCompra.java
+++ src/co/com/facturacion/controller/ControladorCompra.java
@@ -1,5 +1,18 @@
 package co.com.facturacion.controller;
 
+import java.util.ArrayList;
+
+import co.com.facturacion.model.Compra;
+
 public class ControladorCompra {
+	
+	private ArrayList<Compra> compras;
+	
+	public ControladorCompra() {
+		
+		compras = new ArrayList<Compra>();
+		
+	}
+	
 
 }
